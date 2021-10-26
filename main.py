034d6dc8d6d19465e1610f33c8e617e048cbb746
emreustundag1618 <92022448+emreustundag1618@users.noreply.github.com>
2021-10-26 23:19:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,165 +18,172 @@ from tensorflow.keras.models import Model
 from configparser import ConfigParser
 
 from functions import *
+from models_ import *
 
-# parser config
-config_file = "./config.ini"
-cp = ConfigParser()
-cp.read(config_file)
-# default config
-model_name = cp["DEFAULT"].get("model_name")
-learning_rate = cp["DEFAULT"].get("learning_rate")
-min_learning_rate = cp["DEFAULT"].get("min_learning_rate")
-batch_size = cp["DEFAULT"].get("batch_size")
-epochs = cp["DEFAULT"].get("epochs")
-verbose = cp["DEFAULT"].get("verbose")
-img_process_function = cp["DEFAULT"].get("img_process_function")
-isKaggleData = cp["DEFAULT"].get("isKaggleData")
-classification_type = cp["DEFAULT"].get("classification_type")
-classifier = cp["DEFAULT"].get("classifier")
-train_num = cp["DEFAULT"].get("train_num")
-val_num = cp["DEFAULT"].get("val_num")
-show_cv_split_values = cp["DEFAULT"].get("show_cv_split_values")
-feature_number = cp["DEFAULT"].get("feature_number")
-use_fine_tuning = cp["DEFAULT"].get("use_fine_tuning")
-use_chex_weights = cp["DEFAULT"].get("use_chex_weights")
+def main():
 
-input_shape = models_[model_name]["input_shape"]  # input shape required for transfer models
-img_size = input_shape[0]
-
-
-if isKaggleData:
-    data, img_dir = prepare_data_for_kaggle()
-else:
-    data = pd.read_csv("data.csv")
-    img_dir = "images/train"
+    # parser config
+    config_file = "./config.ini"
+    cp = ConfigParser()
+    cp.read(config_file)
+    # default config
+    model_name = cp["DEFAULT"].get("model_name")
+    learning_rate = cp["DEFAULT"].getfloat("learning_rate")
+    min_learning_rate = cp["DEFAULT"].getfloat("min_learning_rate")
+    batch_size = cp["DEFAULT"].getint("batch_size")
+    epochs = cp["DEFAULT"].getint("epochs")
+    verbose = cp["DEFAULT"].getint("verbose")
+    img_process_function = cp["DEFAULT"].get("img_process_function")
+    isKaggleData = cp["DEFAULT"].getboolean("isKaggleData")
+    classification_type = cp["DEFAULT"].get("classification_type")
+    classifier = cp["DEFAULT"].get("classifier")
+    train_num = cp["DEFAULT"].getint("train_num")
+    val_num = cp["DEFAULT"].getint("val_num")
+    show_cv_split_values = cp["DEFAULT"].getboolean("show_cv_split_values")
+    feature_number = cp["DEFAULT"].getint("feature_number")
+    use_fine_tuning = cp["DEFAULT"].getboolean("use_fine_tuning")
+    use_chex_weights = cp["DEFAULT"].getboolean("use_chex_weights")
     
-df_train = data.copy()
-
-if classification_type == "binary":
-    y_col = "image_label"
-else:
-    y_col = "study_label"
-
-if classifier != "ann":
-    class_mode = "raw"
-else:
-    class_mode = "categorical"
-
-# %% Generate Images
-
-image_generator_train = ImageDataGenerator(
-            validation_split=0.2,
-            #rotation_range=20,
-            horizontal_flip = True,
-            zoom_range = 0.1,
-            #shear_range = 0.1,
-            brightness_range = [0.8, 1.1],
-            fill_mode='nearest',
-            preprocessing_function=preprocessing_function
-    )
+    models_ = get_models()
+    input_shape = models_[model_name]["input_shape"]  # input shape required for transfer models
+    img_size = input_shape[0]
     
-image_generator_valid = ImageDataGenerator(validation_split=0.2,
-                                           preprocessing_function=img_adapt_eq)
     
-train_generator = image_generator_train.flow_from_dataframe(
+    if isKaggleData:
+        data, img_dir = prepare_data_for_kaggle()
+    else:
+        data = pd.read_csv("data.csv")
+        img_dir = "images/train"
+        
+    df_train = data.copy()
+    
+    if classification_type == "binary":
+        y_col = "image_label"
+    else:
+        y_col = "study_label"
+    
+    if classifier != "ann":
+        class_mode = "raw"
+    else:
+        class_mode = "categorical"
+    
+    # %% Generate Images
+    
+    image_generator_train = ImageDataGenerator(
+                validation_split=0.2,
+                #rotation_range=20,
+                horizontal_flip = True,
+                zoom_range = 0.1,
+                #shear_range = 0.1,
+                brightness_range = [0.8, 1.1],
+                fill_mode='nearest',
+                preprocessing_function=preprocessing_function
+        )
+        
+    image_generator_valid = ImageDataGenerator(validation_split=0.2,
+                                               preprocessing_function=img_adapt_eq)
+        
+    train_generator = image_generator_train.flow_from_dataframe(
+                dataframe = df_train,
+                directory=img_dir,
+                x_col = 'id',
+                y_col =  y_col,  
+                target_size=(img_size, img_size),
+                batch_size=batch_size,
+                subset='training', seed = 23, class_mode = class_mode) 
+        
+    valid_generator=image_generator_valid.flow_from_dataframe(
             dataframe = df_train,
             directory=img_dir,
             x_col = 'id',
-            y_col =  y_col,  
+            y_col = y_col,
             target_size=(img_size, img_size),
             batch_size=batch_size,
-            subset='training', seed = 23, class_mode = class_mode) 
+            subset='validation', shuffle=False,  seed=23, class_mode = class_mode)
     
-valid_generator=image_generator_valid.flow_from_dataframe(
-        dataframe = df_train,
-        directory=img_dir,
-        x_col = 'id',
-        y_col = y_col,
-        target_size=(img_size, img_size),
-        batch_size=batch_size,
-        subset='validation', shuffle=False,  seed=23, class_mode = class_mode)
-
-# %% Model call, training and evaluating
-
-base_model_class = getattr(
-    importlib.import_module(
-        f"keras.applications.{models_[model_name]['module_name']}"
-        ),
-        model_name)
-          
-img_input = Input(shape = input_shape)
-        
-base_model = base_model_class(
-            include_top = False,
-            input_tensor = img_input,
-            input_shape = input_shape,
-            weights = "imagenet",
-            pooling = "avg")
-base_model.trainable = False
-
-if use_fine_tuning:
-    base_model.trainable = True
-
-
-if (model_name == "DenseNet121") & use_chex_weights:
-    chex_weights_path = '../input/chexnet-weights/brucechou1983_CheXNet_Keras_0.3.0_weights.h5'
-    out = Dense(14, activation='sigmoid')(base_model.output)
-    base_model = Model(inputs=base_model.input, outputs=out)
-    base_model.load_weights(chex_weights_path)
-    x = get_last_conv_layer(model_name).output
-    output = GlobalAveragePooling2D()(x)
+    # %% Model call, training and evaluating
     
-else:
-    x = get_last_conv_layer(model_name).output
-    output = GlobalAveragePooling2D()(x)
-    
-
-if classifier == "ann":
-    if classification_type == "multi":
-        predictions = Dense(len(df_train.study_label.unique()), activation = "softmax", name = "multi_predictions")(output)
-        model = Model(base_model.input, predictions)
-        model.compile(Adam(lr=learning_rate),loss='categorical_crossentropy',metrics=['accuracy'])
-    else:
-        predictions = Dense(len(df_train.image_label.unique()), activation = "softmax", name = "binary_predictions")(output)
-        model = Model(base_model.input, predictions)
-        model.compile(Adam(lr=learning_rate),loss='binary_crossentropy',metrics=['accuracy'])
-        
-
-    rlr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.1, patience = 2, verbose = verbose, 
-                                    min_delta = 1e-4, min_lr = min_learning_rate, mode = 'min')
-    es = EarlyStopping(monitor = 'val_loss', min_delta = 1e-4, patience = 5, mode = 'min', 
-                        restore_best_weights = True, verbose = verbose)
-    ckp = ModelCheckpoint('model.h5',monitor = 'val_loss',
-                          verbose = 0, save_best_only = True, mode = 'min')
-    history = model.fit(
-          train_generator,
-          epochs= epochs,
-          validation_data=valid_generator,
-          callbacks=[es, rlr, ckp],
-          verbose= verbose
-          )
+    base_model_class = getattr(
+        importlib.import_module(
+            f"keras.applications.{models_[model_name]['module_name']}"
+            ),
+            model_name)
+              
+    img_input = Input(shape = input_shape)
+            
+    base_model = base_model_class(
+                include_top = False,
+                input_tensor = img_input,
+                input_shape = input_shape,
+                weights = "imagenet",
+                pooling = "avg")
+    base_model.trainable = False
     
     if use_fine_tuning:
-        model.save_weights(f"{model_name}-model.h5")
-        
-    plot_tl_metrics()
-    get_confusion_matrix()
-        
-
-# X train and test extractions for ML classifiers, e.g SVM
-
-if classifier == "svm":
-    output = Dense(feature_number, activation = "relu", name = "features")(output)
-    model = Model(base_model.input, output)
+        base_model.trainable = True
     
-    #  Feature extract for ML classifiers
-    x_tr, x_val, y_tr, y_val = generate_images_for_SVM(train_num, val_num)
-    x_train, x_test, y_train, y_test = extract_features_from_images(x_tr, x_val, y_tr, y_val)
-    print_feature_shapes(x_train, x_test, y_train, y_test)
-    show_SVM_results(x_train, x_test, y_train, y_test)
-
-    if show_cv_split_values:
-        cross_val_score_plot(number_of_top = 7)
     
-    get_confusion_matrix()
+    if (model_name == "DenseNet121") & use_chex_weights:
+        chex_weights_path = '../input/chexnet-weights/brucechou1983_CheXNet_Keras_0.3.0_weights.h5'
+        out = Dense(14, activation='sigmoid')(base_model.output)
+        base_model = Model(inputs=base_model.input, outputs=out)
+        base_model.load_weights(chex_weights_path)
+        x = get_last_conv_layer(model_name).output
+        output = GlobalAveragePooling2D()(x)
+        
+    else:
+        x = get_last_conv_layer(model_name).output
+        output = GlobalAveragePooling2D()(x)
+        
+    
+    if classifier == "ann":
+        if classification_type == "multi":
+            predictions = Dense(len(df_train.study_label.unique()), activation = "softmax", name = "multi_predictions")(output)
+            model = Model(base_model.input, predictions)
+            model.compile(Adam(lr=learning_rate),loss='categorical_crossentropy',metrics=['accuracy'])
+        else:
+            predictions = Dense(len(df_train.image_label.unique()), activation = "softmax", name = "binary_predictions")(output)
+            model = Model(base_model.input, predictions)
+            model.compile(Adam(lr=learning_rate),loss='binary_crossentropy',metrics=['accuracy'])
+            
+    
+        rlr = ReduceLROnPlateau(monitor = 'val_loss', factor = 0.1, patience = 2, verbose = verbose, 
+                                        min_delta = 1e-4, min_lr = min_learning_rate, mode = 'min')
+        es = EarlyStopping(monitor = 'val_loss', min_delta = 1e-4, patience = 5, mode = 'min', 
+                            restore_best_weights = True, verbose = verbose)
+        ckp = ModelCheckpoint('model.h5',monitor = 'val_loss',
+                              verbose = 0, save_best_only = True, mode = 'min')
+        history = model.fit(
+              train_generator,
+              epochs= epochs,
+              validation_data=valid_generator,
+              callbacks=[es, rlr, ckp],
+              verbose= verbose
+              )
+        
+        if use_fine_tuning:
+            model.save_weights(f"{model_name}-model.h5")
+            
+        plot_tl_metrics()
+        get_confusion_matrix()
+            
+    
+    # X train and test extractions for ML classifiers, e.g SVM
+    
+    if classifier == "svm":
+        output = Dense(feature_number, activation = "relu", name = "features")(output)
+        model = Model(base_model.input, output)
+        
+        #  Feature extract for ML classifiers
+        x_tr, x_val, y_tr, y_val = generate_images_for_SVM(train_num, val_num)
+        x_train, x_test, y_train, y_test = extract_features_from_images(x_tr, x_val, y_tr, y_val)
+        print_feature_shapes(x_train, x_test, y_train, y_test)
+        show_SVM_results(x_train, x_test, y_train, y_test)
+    
+        if show_cv_split_values:
+            cross_val_score_plot(number_of_top = 7)
+        
+        get_confusion_matrix()
+        
+if __name__ == "__main__":
+    main()
